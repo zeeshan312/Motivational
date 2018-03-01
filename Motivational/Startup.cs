48f47b78a6cffd59c455d98e6573a9af81008ef9
whinstone1 <zeemurrawat@hotmail.com>
2018-03-01 14:39:15 +0500

    Add project files.

---
+++ Motivational/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Motivational.Startup))]
+namespace Motivational
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
