c0686a34404a86b3edc0323d5985eaea6d58dd54
whinstone1 <zeemurrawat@hotmail.com>
2018-03-02 12:55:37 +0500

    adding shrek

---
+++ Motivational/Controllers/ManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Motivational.Models;
+
+namespace Motivational.Controllers
+{
+    public class ManagerController : Controller
+    {
+        //Get : Accounts/Ascending
+        public ActionResult Ascending()
+        {
+            var admin   = new Accounts() { Company "whinstone" };
+            return View(admin);
+
+        }
+
+    }
+    
+}
