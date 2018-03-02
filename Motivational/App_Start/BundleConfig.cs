6da6398956f413f4288a39c9bfc1aa678af41117
whinstone1 <zeemurrawat@hotmail.com>
2018-03-02 13:07:11 +0500

    bootstrap-cyborg

--- Motivational/App_Start/BundleConfig.cs
+++ Motivational/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Motivational
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
